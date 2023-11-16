0e9a63d3ace22c2429f31308187c7bfb873b86f4
Valentin Dury <valentin03dury@gmail.com>
2023-11-16 12:22:03 +0100

    [ADD] Display last played move in color.
    Signed-off-by: Valentin Dury <valentin03dury@gmail.com>

--- visu.py
+++ visu.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 import sys
+from main import bcolors
 
 def createMap() -> list[list[int]]:
     map = []
@@ -9,28 +10,36 @@ def createMap() -> list[list[int]]:
             map[i].append(0)
     return map
 
-def displayMap(map: list[list[int]]):
+def displayMap(map: list[list[int]], player : int):
     for i in range(0, 20):
         for j in range(0, 20):
             if map[i][j] == 1:
                 print("x", end="")
             elif map[i][j] == 2:
                 print("o", end="")
+            elif map[i][j] == 3:
+                print(f"{bcolors.BOLD}{bcolors.OKGREEN}X{bcolors.ENDC}", end="")
+                if player == 2:
+                    map[i][j] = 1
+            elif map[i][j] == 4:
+                print(f"{bcolors.BOLD}{bcolors.FAIL}O{bcolors.ENDC}", end="")
+                if player == 1:
+                    map[i][j] = 2
             else:
                 print(".", end="")
         print()
 
 def main():
     map = createMap()
-    displayMap(map)
     with open(sys.argv[1], "r") as file:
-        for line in file:
+        for i, line in enumerate(file):
             x, y, player = line.split(",")
             x = int(x)
             y = int(y)
-            map[x][y] = int(player)
-            displayMap(map)
-            input()
+            player = int(player)
+            map[x][y] = player + 2
+            input(f"Coup {i + 1}:")
+            displayMap(map, player)
 
 if __name__ == "__main__":
     main()
